072d16271b01b8557de30eedf3599e29dc2aa463
AlannaKeila <88459750+AlannaKeila@users.noreply.github.com>
2023-03-15 11:56:05 -0300

    Update calendario.php

--- calendario.php
+++ calendario.php
@@ -1,42 +1,65 @@
-<h1><?php echo "Calendário 3C"; ?> </h1>
+<h1><?php echo "Calendario 3C"; ?></h1>
+<?php date_default_timezone_set('America/Sao_Paulo');
 
-<?php function linha($semana){
-    echo "<tr>
-             <td>{$semana[0]}</td>
-             <td>{$semana[1]}</td>
-             <td>{$semana[2]}</td>
-             <td>{$semana[3]}</td>
-             <td>{$semana[4]}</td>
-             <td>{$semana[5]}</td>
-             <td>{$semana[6]}</td>
-         </tr>";
+$hoje = date('d/m/Y');
+if ($dia == $hoje){
+	echo "<strong> $hoje </strong>";
 }
 
-function calendario(){
-    $dia = 1;
-    $semana = array();
+$hora = date('h');
+echo "<br> Agora são " . date('H:i:s') . "<br>";
 
-while ($dia <= 31) {
-array_push($semana, $dia);
-if (count($semana) == 7) {
-linha($semana);
-$semana = array(); }
-$dia++;
+	if (($hora >= 5) && ($hora <=12)){
+		echo "Bom dia!";
+	}
+	if (($hora > 12) && ($hora <=17)){
+		echo "Boa tarde!";
+	}	
+	if (($hora > 17) && ($hora <5)){
+		echo "Boa noite!";
+	}
+
+
+?>
+<?php function linha($semana)
+{
+	echo "<tr>";
+	for ($i = 0; $i <= 6; $i++) {
+		if (isset($semana[$i])) {
+			echo "<td>{$semana[$i]}</td>";
+		} else {
+			echo "<td></td>";
+		}
+	}
+	echo "</tr>";
 }
+
+function calendario()
+{
+	$dia = 1;
+	$semana = array();
+	while ($dia <= 31) {
+		array_push($semana, $dia);
+		if (count($semana) == 7) {
+			linha($semana);
+			$semana = array();
+		}
+		$dia++;
+	}
+	
 }
 ?>
 
-<br>
- <table border="1">
- <tr>
- <th>Dom</th>
- <th>Seg</th>
- <th>Ter</th>
- <th>Qua</th>
- <th>Qui</th>
- <th>Sex</th>
- <th>Sáb</th>
- </tr>
- <?php calendario(); ?>
- 
- </table>
+<table border="1">
+	<tr>
+		<th>Dom</th>
+		<th>Seg</th>
+		<th>Ter</th>
+		<th>Qua</th>
+		<th>Qui</th>
+		<th>Sex</th>
+		<th>Sáb</th>
+	</tr>
+	
+	<?php calendario(); ?>
+</table>
