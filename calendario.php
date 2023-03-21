ce6f9ec0c3e4d4c1cbaa8ec39fcbf86aa764f657
AlannaKeila <88459750+AlannaKeila@users.noreply.github.com>
2023-03-21 13:15:52 -0300

    Update calendario.php

--- calendario.php
+++ calendario.php
@@ -1,7 +1,8 @@
 <h1><?php echo "Calendario 3C"; ?></h1>
 <?php date_default_timezone_set('America/Sao_Paulo');
-
+//exibir hora atual
 echo "<br> Agora são " . date('H:i:s') . "<br>";
+//exibir frase de acordo com a hora
 $hora = date('H');
 	if ($hora >= 5 && $hora <=12){
 		echo "Bom dia!";
@@ -12,20 +13,29 @@ $hora = date('H');
 	if ($hora > 17 && $hora <5){
 		echo "Boa noite!";
 	}
+	
 ?>
 
 <?php function linha($semana){
   echo "<tr>";
   for ($i = 0; $i <= 6; $i++) {
-    if (isset($semana[$i])) {
+	//data atual em negrito
+	if ((isset($semana[$i])) && ($semana[$i] == date('d'))){
+		echo "<td><b>{$semana[$i]}</b></td>";
+	}
+	//exibir domingos na cor vermelha
+	else if ((isset($semana[$i])) && ($semana[$i] == $semana['0'])){
+		echo "<td><font color='red'>{$semana[$i]}</font></td>";
+	//exibir sábados em negrito 
+	}else if ((isset($semana['6'])) && ($semana[$i] == $semana['6'])){
+		echo "<td><b>{$semana[$i]}</b></td>";
+    }else if (isset($semana[$i])) {
     echo "<td>{$semana[$i]}</td>";
- } else {
+   } else {
    echo "<td></td>";
- }
+  } 
 }
-  
 }
-
 function calendario()
 {
 	$dia = 1;
@@ -39,6 +49,7 @@ function calendario()
 		$dia++;
 	}
 	linha($semana);
+	
 	}
 ?>
 
